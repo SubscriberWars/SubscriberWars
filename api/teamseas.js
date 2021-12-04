af0dd8e363540924bd4aa416569f2b4abdc9656b
SubscriberWars <75580135+SubscriberWars@users.noreply.github.com>
2021-12-04 15:29:45 +0000

    Create teamseas.js

---
+++ api/teamseas.js
@@ -0,0 +1,15 @@
+const express=require('express'),
+app=express(),
+fetch=require('node-fetch');
+const path=require('path')
+
+app.get('/count',(req,resp)=>{
+  resp.setHeader('Access-Control-Allow-Origin','https://subscriberwars.space');
+  fetch("https://tscache.com/donation_total.json")
+
+  .then(res=>res.json()).then(data=>{
+    resp.json({
+      count:(data.count),
+    })
+  })
+})
